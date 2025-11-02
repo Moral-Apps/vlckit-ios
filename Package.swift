2eabba24f13915dddd5a41e7bd52650eeb39c21a
Pedro Morgado Alarcón <p.morgado107@gmail.com>
2025-11-02 11:56:08 +0100

    First version of package

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let vlcFramework = Target.binaryTarget(name: "VLCKitFramework", url: "https://github.com/Moral-Apps/vlckit-ios/releases/download/3.6.0/VLCKit.xcframework.zip", checksum: "0e758e6418cb68ab1cb85417fc72ee6cddfb7a7caa0ccd18ed5d8d0c53f5194a")
+
+let package = Package(
+    name: "vlckit-ios",
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v13),
+        .macOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "VLCKit",
+            targets: ["VLCKit"]),
+    ],
+    dependencies: [],
+    targets: [
+        vlcFramework,
+        .target(
+            name: "VLCKit",
+            dependencies: [
+                .target(name: "VLCKitFramework")
+            ], linkerSettings: [
+                .unsafeFlags(["-ObjC"])
+            ]),
+    ]
+)
